1a6223746ed7f1fc9c48970ca667a66fb720a29b
UzielSilva <uziel.silva.espino@gmail.com>
2015-04-05 22:24:56 -0500

    Cleaning code.

--- src/main/java/com/simplej/vc/util/Main.java
+++ src/main/java/com/simplej/vc/util/Main.java
@@ -1,29 +1,28 @@
+/**
+ * Created by Uziel on 04/04/2015.
+ */
 package com.simplej.vc.util;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.stage.StageStyle;
+import org.controlsfx.dialog.Dialogs;
+import org.apache.maven.artifact.versioning.DefaultArtifactVersion;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.util.List;
 import java.util.Properties;
 
-/**
- * Created by Uziel on 04/04/2015.
- */
 public class Main extends Application {
-    public Properties config;
+    public static final Properties config;
 
-    public void init(){
-
+    static {
         config = new Properties();
         try {
-            InputStream stream = getClass().getClassLoader().getResourceAsStream("com/simplej/core.properties");
+            InputStream stream = Main.class.getResourceAsStream("/com/simplej/core.properties");
             try {
                 config.load(stream);
             }
@@ -32,20 +31,46 @@ public class Main extends Application {
             }
         }
         catch (IOException ex) {
-            System.out.print("...");
             ex.printStackTrace();
         }
     }
     @Override
+
     public void start(Stage primaryStage) throws Exception{
-        init();
-        String selectorPath = config.getProperty("fxml.selector.path");
-        Parent root = FXMLLoader.load(getClass().getResource(selectorPath));
-        primaryStage.initStyle(StageStyle.TRANSPARENT);
-        primaryStage.setTitle("Selector");
-        Scene scene = new Scene(root);
-        scene.setFill(Color.TRANSPARENT);
-        primaryStage.setScene(scene);
+
+        DefaultArtifactVersion minVersion = new DefaultArtifactVersion("1.8");
+        DefaultArtifactVersion version = new DefaultArtifactVersion(System.getProperty("java.version"));
+
+        if (version.compareTo(minVersion) < 0) {
+            Dialogs.create()
+                    .owner(primaryStage)
+                    .title("SimpleJ DevKit")
+                    .masthead("Error in Java version.")
+                    .message(String.format("Should be %s or greater", minVersion))
+                    .showError();
+            System.exit(2);
+        }
+        List<String> args = getParameters().getRaw();
+
+        boolean countMain;
+        boolean countVBI;
+        boolean countSFI;
+        boolean profileVBI;
+
+        for (String arg : args) {
+            if (arg.equals("-main"))
+                countMain = true;
+            else if (arg.equals("-vbi"))
+                countVBI = true;
+            else if (arg.equals("-sfi"))
+                countSFI = true;
+            else if (arg.equals("-vbiProf"))
+                profileVBI = true;
+            else {
+                System.err.println("Unknown option: " + arg);
+                System.exit(2);
+            }
+        }
         SelectorFX.setStage(primaryStage);
         primaryStage.show();
     }
--- src/main/java/com/simplej/vc/util/SelectorFX.java
+++ src/main/java/com/simplej/vc/util/SelectorFX.java
@@ -7,27 +7,42 @@ package com.simplej.vc.util;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
 
 public class SelectorFX implements Initializable {
 
+    private static Parent root;
     @FXML
     private ListView<String> projectList;
     @FXML
     private TextField newProject;
     private static Stage stage;
+    @FXML
+    private void cancelAction(){
+        stage.close();
+    }
 
-    public static void setStage(Stage theStage){
-        stage = theStage;
+    public static void setStage(Stage primaryStage) throws IOException {
+        String selectorPath = Main.config.getProperty("fxml.selector.path");
+        Parent root = FXMLLoader.load(Main.class.getResource(selectorPath));
+        primaryStage.initStyle(StageStyle.TRANSPARENT);
+        primaryStage.setTitle("Selector");
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        primaryStage.setScene(scene);
+        stage = primaryStage;
     }
 
     @Override
@@ -38,5 +53,4 @@ public class SelectorFX implements Initializable {
         ObservableList<String> projects = FXCollections.observableArrayList(s);
         projectList.setItems(projects);
     }
-
 }
